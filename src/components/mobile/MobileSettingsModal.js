ea791bbd60c6be1cfb2e71924cb56194f1db92f0
AmanMayank <aman.msingh87@gmail.com>
2024-09-02 18:07:45 +0530

    settingsUiFix

--- src/components/mobile/MobileSettingsModal.js
+++ src/components/mobile/MobileSettingsModal.js
@@ -27,8 +27,8 @@ const MobileSettingsModal = ({
   handleMilestone,
 }) => {
   return (
-    <div className="top-0 left-0 right-0 bottom-0 w-full h-[100vh]  fixed z-30 mx-auto flex backdrop-blur items-center justify-center ">
-      <div className="bg-white w-full border-2 p-4 border-black align-baseline relative">
+    <div className="top-0 left-0 right-0 bottom-0 w-full h-[100vh]  fixed z-30 mx-auto flex backdrop-blur items-end justify-end ">
+      <div className="bg-white h-auto w-full border-2 p-4 border-black align-baseline relative transition duration-1000 ease-in-out">
         <div className="w-full  flex items-center justify-between">
           <h2 className="font-bold text-2xl">Puzzle Settings</h2>
           <div
