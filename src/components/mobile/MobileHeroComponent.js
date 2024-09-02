e5a2211aad6fa40f9b449f979523a03c56b635a6
AmanMayank <aman.msingh87@gmail.com>
2024-09-02 20:00:14 +0530

    WIP

--- src/components/mobile/MobileHeroComponent.js
+++ src/components/mobile/MobileHeroComponent.js
@@ -63,9 +63,9 @@ const MobileHeroComponent = ({
 
   const customLayout = {
     default: [
-      " Q W E R T Y U I O P ",
-      " A S D F G H J K L ",
-      "123 Z X C V B N M {bksp}",
+      "Q W E R T Y U I O P",
+      "A S D F G H J K L",
+      "Z X C V B N M {bksp}",
     ],
   };
 
